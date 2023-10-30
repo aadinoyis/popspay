6dbbfb45469fd8abf4d55836f6972399430e245b
aadinoyis <abdulwahabadinoyis@gmail.com>
2023-10-30 17:00:40 +0100

    route updated

--- app/api/user/route.tsx
+++ app/api/user/route.tsx
@@ -3,15 +3,12 @@ import { cookies } from "next/headers";
 import * as jose from "jose";
 import { supabase } from "@/app/utils/Supabase";
 
-export async function userId() {
+export async function POST(req: Request) {
   const token = cookies().get("hanko")?.value;
   const payload = jose.decodeJwt(token ?? "");
 
-  return payload.sub;
-}
+  const userID = payload.sub;
 
-export async function POST(req: Request) {
-  const userID = await userId();
   const { email } = await req.json();
   if (userID) {
     
@@ -24,6 +21,25 @@ export async function POST(req: Request) {
     .from('users')
     .upsert({ user_id: userID, email: email })
     .select()
+
+    return NextResponse.json({ message: data }, { status: 200 });
+  } else {
+    return NextResponse.json({ error: "Not Found" }, { status: 404 });
+  }
+}
+
+export async function GET(req: Request) {
+  const token = cookies().get("hanko")?.value;
+  const payload = jose.decodeJwt(token ?? "");
+
+  const userID = payload.sub;
+
+  if (userID) {
+    // Create a new user if the user is not found
+    const { data, error } = await supabase
+    .from('users')
+    .select('*')
+    .eq('user_id', userID);
 
     return NextResponse.json({ message: data }, { status: 200 });
   } else {
