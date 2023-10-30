09e42cee6702f47fe70395344b4ec3da7063b57f
aadinoyis <abdulwahabadinoyis@gmail.com>
2023-10-30 19:33:52 +0100

    not understanding this

--- app/api/user/route.tsx
+++ app/api/user/route.tsx
@@ -3,40 +3,39 @@ import { cookies } from "next/headers";
 import * as jose from "jose";
 import { supabase } from "@/app/utils/Supabase";
 
-// export async function POST(req: Request) {
-//   const token = cookies().get("hanko")?.value;
-//   const payload = jose.decodeJwt(token ?? "");
+export async function POST(req: Request) {
+  const token = cookies().get("hanko")?.value;
+  const payload = jose.decodeJwt(token ?? "");
 
-//   const userID = payload.sub;
+  const userID = payload.sub;
 
-//   const { email } = await req.json();
-//   if (userID) {
+  const { email } = await req.json();
+  if (userID) {
     
-//     if (typeof email !== "string" || email.length === 0) {
-//       throw new Error("That can't be an email");
-//     }
+    if (typeof email !== "string" || email.length === 0) {
+      throw new Error("That can't be an email");
+    }
 
-//     // Create a new user if the user is not found
-//     const { data, error } = await supabase
-//     .from('users')
-//     .upsert({ user_id: userID, email: email })
-//     .select()
+    // Create a new user if the user is not found
+    const { data, error } = await supabase
+    .from('users')
+    .upsert({ user_id: userID, email: email })
+    .select()
 
-//     return NextResponse.json({ message: data }, { status: 200 });
-//   } else {
-//     return NextResponse.json({ error: "Not Found" }, { status: 404 });
-//   }
-// }
+    return NextResponse.json({ message: data }, { status: 200 });
+  } else {
+    return NextResponse.json({ error: "Not Found" }, { status: 404 });
+  }
+}
 
 export async function GET(req: Request) {
-
   try {
     const token = cookies().get("hanko")?.value;
     const payload = jose.decodeJwt(token ?? "");
 
     const userID = payload.sub;
 
-    return NextResponse.json({ message: userID}, { status: 200 });
+    return NextResponse.json({ message: userID }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: "Not Found" }, { status: 404 });
   }
