a58a3924cd8fc7cdfdb0176e583b6039661f72b1
aadinoyis <abdulwahabadinoyis@gmail.com>
2023-10-31 01:10:24 +0100

    array or object

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -28,8 +28,9 @@ const Dashboard = async () => {
       }
   
       const data = await response.json();
-      console.log(data);
-      setUser(data);
+      
+      console.log(data[0].balance);
+      // setUser(data);
     } catch (error) {
       console.error('Error:', error);
       // You can handle the error or provide user feedback here
@@ -45,7 +46,7 @@ const Dashboard = async () => {
     <div className='dashboard'>
       <Nav/>
 
-      <Balance balance={user ? user[0].balance : ''}/>
+      <Balance balance={'0'}/>
       
       {n == 'action' && <ActionDocker/>}
       {n == 'notify' && <NotifyDocker/>}
